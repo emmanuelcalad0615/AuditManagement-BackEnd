9ca4dfa83071b4c365b74fda22077bf41f212178
emmanuelcalad0615 <emmanuelcalad@gmail.com>
2025-04-14 02:32:08 -0500

    Errors solution

--- model/Sector.py
+++ model/Sector.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from repository.connector.Connector import Base
 
-class Aspecto(Base):
+class Sector(Base):
     __tablename__ = "sector"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
